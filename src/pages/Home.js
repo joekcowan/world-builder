83dec86d7e33c5f357828512da29245d40553596
joekcowan <joekcowan@gmail.com>
2023-03-24 20:29:37 -0400

    updated favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ function Home() {
             <Card.Text>
               <h4>You have reached my world building app. This is an app designed to help gamers and storyteller's build out their ideas. To get started click on an option for your starting point.</h4>
             </Card.Text>
-            <Link to="/worlds"><Button variant="primary" className='m-1'>Add A World</Button></Link>
-            <Link to="/characters"><Button variant="primary" className='m-1'>Build a world</Button></Link>
+            <Link to="/worlds"><Button variant="success" className='m-1'>Add A World</Button></Link>
+            <Link to="/characters"><Button variant="success" className='m-1'>Add a character</Button></Link>
           </Card.Body>
         </Card>
 
